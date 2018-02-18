3643234cca4d4fdb476849b7e3775989c9274120
KellyYuotngHe <yhe29@u.rochester.edu>
2018-02-18 02:46:56 -0500

    web

---
+++ app.py
@@ -0,0 +1,23 @@
+import random
+import string
+import os.path
+import cherrypy
+import mc
+
+
+class Generator(object):
+    @cherrypy.expose
+    def index(self):
+        return open("index.html")
+
+    @cherrypy.expose
+    def generate(self, length = 3):
+        return mc.test(length)
+
+    @cherrypy.expose
+    def example(self):
+        return open("example.html")
+
+
+if __name__ == '__main__':
+    cherrypy.quickstart(Generator())
--- mc.py
+++ mc.py
@@ -3,6 +3,9 @@ from numpy import cumsum, sum, searchsorted
 from numpy.random import rand
 from random import randint
 import pickle
+import random
+import string
+
 
 def predict(phrase):
     probs = sorted(word_dict[phrase].iteritems(), key=lambda (k,v): (v,k), reverse = True)
@@ -10,21 +13,16 @@ def predict(phrase):
     index = searchsorted(cumsum(probs.values()), target)[0]
     return probs[probs.keys()[index]]
 
-# Generates n characters from start.
-def generate(self, start, n):
+def generate(start, n):
     result = start
-
-    # Foreach in n
     for i in range(n):
-        # Get the next letter
-        new = self.predict(start)
-
-        # Add that letter to our output
+        new = predict(tuple(start.split(" ")))
         result += new
-
-        # Our next start should be the new, minus the first letter of the old. So "abc" -> "abcd" -> "bcd"
         start = start[1:] + new
     return result
+
+def test(length):
+    return ''.join(random.sample(string.hexdigits, int(length)))
 
 def main():
     word_dict = {}
@@ -32,7 +30,7 @@ def main():
         word_dict = pickle.load(f)
 
     print word_dict.keys()[1]
-    print predict(("Young", ))
+    print generate("I", 2)
 
 if __name__ == "__main__":
     main()
